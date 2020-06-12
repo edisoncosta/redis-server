ea724cd88465d9687cb35f50504f78c43ad51083
Edison Costa <edison@agiuscloud.com>
2020-06-12 17:06:05 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /tmp
+
+apt-update
+apt-get install build-essential tcl
+
+wget http://download.redis.io/releases/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+
+cd redis-stable
+make && make install
+
+cd utils/
+source install_server.sh
